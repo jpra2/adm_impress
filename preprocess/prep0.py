6388d89140ed280a089995dbfc8cb48175f51e6e
jprandrade <jprandrade2@gmail.com>
2019-10-05 11:24:01 -0300

    add_preprocess0_5

--- preprocess/prep0.py
+++ preprocess/prep0.py
@@ -37,6 +37,7 @@ class Preprocess0:
     def __init__(self, M):
         self.set_permeability_and_phi(M)
         self.set_area_hex_structured(M)
+        self.set_k_harm_hex_structured(M)
 
     def set_area_hex_structured(self, M):
 
@@ -44,37 +45,54 @@ class Preprocess0:
             indice = np.where(normals[:,ind] == 1)[0][0]
             nos = nodes_faces[indice]
             normas = []
+            vetores = []
             maior = 0
             ind_maior = 0
             for i in range(3):
                 v = coord_nodes[nos[0]] - coord_nodes[nos[i+1]]
+                vetores.append(v)
                 normas.append(np.linalg.norm(v))
                 if normas[i] > maior:
                     maior = normas[i]
                     ind_maior = i
 
             del normas[ind_maior]
+            del vetores[ind_maior]
 
             area = normas[0] * normas[1]
+            vetores = np.absolute(np.array(vetores))
+            normas = np.array(normas)
 
-            return area
+            return area, vetores, normas
 
+        unis = np.array([np.array([1,0,0]), np.array([0,1,0]), np.array([0,0,1])])
         faces = M.faces.all
         n_faces = len(faces)
         normals = np.absolute(M.faces.normal[:])
         nodes_faces = M.faces.bridge_adjacencies(faces, 2, 0)
         coord_nodes = M.nodes.center(M.nodes.all)
         areas = []
-        hs = []
+        hs = np.zeros(3)
         for i in range(3):
-            area = get_area(i, normals, nodes_faces, coord_nodes)
+            area, vetores, modulos = get_area(i, normals, nodes_faces, coord_nodes)
+            unis2 = np.zeros([2,3])
+            unis2[0] = vetores[0]/modulos[0]
+            unis2[1] = vetores[1]/modulos[1]
             areas.append(area)
+            for j in range(2):
+                if np.allclose(unis2[j], unis[0]):
+                    hs[0] = modulos[j]
+                elif np.allclose(unis2[j], unis[1]):
+                    hs[1] = modulos[j]
+                elif np.allclose(unis2[j], unis[2]):
+                    hs[2] = modulos[j]
 
         areas = np.array(areas)
-
         all_areas = np.dot(normals, areas)
+        dist_cent = np.dot(normals, hs)
 
         M.data.variables[direc.variables_impress['area']] = all_areas
+        M.data.variables[direc.variables_impress['dist_cent']] = dist_cent
 
     def set_permeability_and_phi(self, M):
         data_loaded = direc.data_loaded
@@ -118,19 +136,35 @@ class Preprocess0:
         values = np.repeat(0.3, n)
         M.data.variables[direc.variables_impress['poro']] = values
 
-    def set_k_harm(self, M):
+    def set_k_harm_hex_structured(self, M):
         '''
         considerando malha estruturada
         '''
-        vols_viz_faces = M.data.entities_lv0[direc_impress.entities_lv0_0[1]]
-        internal_faces = M.data.entities_lv0[direc_impress.entities_lv0_0[0]]
+        normals = np.absolute(M.faces.normal[:])
+        vols_viz_faces = M.data.elements_lv0[direc_impress.entities_lv0_0[1]]
+        internal_faces = M.data.elements_lv0[direc_impress.entities_lv0_0[0]]
         centroids_volumes = M.data.centroids[direc.entities_lv0[3]]
-        ks = M.data.variables[direc.variables_impress['permeability']]
+        ks = M.data.variables[direc.variables_impress['permeability']].copy()
+        # ks = ks.reshape([len(ks), 3, 3])
+
         areas = M.data.variables[direc.variables_impress['area']]
+        k_harm = np.zeros(len(areas))
+
+        import pdb; pdb.set_trace()
+
+        vols_viz_internal_faces = M.data.elements_lv0[direc.entities_lv0_0[2]]
+        vols_viz_boundary_faces = M.data.elements_lv0[direc.entities_lv0_0[3]]
+
+        import pdb; pdb.set_trace()
+        shape_ks_0 = [vols_viz_internal_faces.shape[0], vols_viz_internal_faces.shape[1], 9]
 
 
+        ks_vols_viz_internal_faces = np.zeros(shape_ks_0)
+        ks_vols_viz_internal_faces[:,0] = ks[vols_viz_internal_faces[:,0]]
+        ks_vols_viz_internal_faces[:,1] = ks[vols_viz_internal_faces[:,1]]
+
+        for i, k01 in enumerate(ks_vols_viz_internal_faces):
+            pass
 
 
-        # TODO: terminar essa funcao
-
-    def set_dist_centroid_hex_structured(self, M):
+        import pdb; pdb.set_trace()
