eb63e8429749d93c837051f4e58d60976e8b2210
JP <jprandrade2@gmail.com>
2019-12-06 12:16:26 -0300

    pass_test_monophasic

--- testting1.py
+++ testting1.py
@@ -105,11 +105,13 @@ import pdb
 from packs.running.initial_mesh_properties import initial_mesh
 from packs.pressure_solver.fine_scale_tpfa import FineScaleTpfaPressureSolver
 
-load = True
-convert = True
+load = False
+convert = False
 M, elements_lv0, data_impress, wells = initial_mesh(load=load, convert=convert)
 tpfa_solver = FineScaleTpfaPressureSolver(data_impress, elements_lv0, wells)
 tpfa_solver.run()
+data_impress.update_variables_to_mesh()
+M.core.print(file='test', extension='.vtk', config_input='input_cards/print_settings0.yml')
 
 
 
