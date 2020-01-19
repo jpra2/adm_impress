b86b1edd6c4057f02256d2a198b97fb41d25b57a
DudaGalindo <meduardagalindo8@gmail.com>
2020-01-19 18:25:19 -0300

    Compositional basic run code structured until the first IMPEC part

--- testting_compositional.py
+++ testting_compositional.py
@@ -1,7 +1,8 @@
 import pdb
 from packs.running.compositional_initial_mesh_properties import initial_mesh
-from packs.compositional import CompositionalIMPEC
+from packs.compositional.CompositionalIMPEC import CompositionalTPFA
 from packs.directories import data_loaded
+from stability_check import StabilityCheck
 import scipy.sparse as sp
 import numpy as np
 import time
@@ -9,7 +10,13 @@ import time
 load = data_loaded['load_data']
 convert = data_loaded['convert_english_to_SI']
 
-M, elements_lv0, data_impress, wells, fluid_properties = initial_mesh(load=load, convert=convert)
-tpfa_solver = CompositionalIMPEC.update_transmissibility(M, data_impress, elements_lv0, fluid_properties)
+M, elements_lv0, data_impress, wells = initial_mesh(load=load, convert=convert)
+fluid_properties = StabilityCheck(w,Bin,R,Tc,Pc,T,P,C7)
+fluid_properties.run(z,Mw)
+t = 0
+while t < tfinal:
+    CompositionalTPFA(M, fluid_properties, elements_lv0, load)
+    #check stability and perform flash calculation
+
 #data_impress.update_variables_to_mesh()
 #M.core.print(file='test'+ str(n), extension='.vtk', config_input='input_cards/print_settings0.yml')
