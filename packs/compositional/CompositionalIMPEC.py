84f84ebc799b1e4456d04394b7190d4ee23a8236
Eduarda <meduardagalindo8@gmail.com>
2020-01-23 16:23:39 -0300

    Includded functions to the remaining partial derivative terms, but so far it's working without them

--- packs/compositional/CompositionalIMPEC.py
+++ packs/compositional/CompositionalIMPEC.py
@@ -2,6 +2,7 @@ from ..directories import data_loaded
 from ..data_class.data_manager import DataManager
 from ..utils import relative_permeability2, phase_viscosity
 from .. import directories as direc
+import scipy.sparse as sp
 import numpy as np
 
 #Next step: por os parâmetros de entrada e entender como a class fluid_properties vai entrar
@@ -31,7 +32,7 @@ class CompositionalTPFA(DataManager):
         self.update_saturations(data_impress, wells, fluid_properties)
         self.update_relative_permeabilities(fluid_properties)
         self.update_phase_viscosities(fluid_properties)
-        self.update_transmissibility(M, elements_lv0, fluid_properties)
+        self.update_transmissibility(M, data_impress, elements_lv0, fluid_properties)
 
         # self.M = M
         # self.elements_lv0 = elements_lv0
@@ -42,6 +43,7 @@ class CompositionalTPFA(DataManager):
         # self.solver = SolverSp()
 
     def get_water_data(self, data_loaded, fluid_properties):
+        ''' Attention: Water assumed to be incompressible '''
         fluid_properties.rho_W = data_loaded['compositional_data']['rho_W']
         self.mi_W = data_loaded['compositional_data']['mi_W']
         fluid_properties.Mw_w = data_loaded['compositional_data']['Mw_w']
@@ -102,26 +104,59 @@ class CompositionalTPFA(DataManager):
         self.phase_viscosities[0,1,:] = self.phase_viscosities_oil_and_gas[0,1,:]
         self.phase_viscosities[0,2,:] = self.mi_W
 
-    def update_transmissibility(self, M, elements_lv0, fluid_properties):
+    def dVtdNk(self):
+        """n sei como vou fazer isso"""
+        return dVtdNk
+
+    def dVtdP(self):
+        """é o mesmo fumo do de cima"""
+        return dVtdP
+
+    def update_deltaT(self):
+        """include CFL condition and flux calculations"""
+        
+    def update_transmissibility(self, M, data_impress, data_loaded, elements_lv0, fluid_properties):
         v0 = elements_lv0['neig_internal_faces']
         pretransmissibility_faces = M.data[M.data.variables_impress['pretransmissibility']]
-        pretransmissibility_internal_faces = pretransmissibility_faces[internal_faces]
-
+        pretransmissibility_internal_faces = pretransmissibility_faces[self.internal_faces]
+        n_volumes = data_impress.len_entities['volumes']
         mobilities = self.relative_permeabilities / self.phase_viscosities
+        dVtdNk = self.dVtdNk()
+        dVtdP = self.dVtdP()
+        porosity = data_loaded['compositional_data']['Porosity']
+        cf = data_loaded['compositional_data']['rock_compressibility']
 
-        print('Yay')
-        t0 = (self.component_molar_fractions * self.phase_molar_densities *\
-              mobilities).sum(axis=1).sum(axis=0)
-        # import pdb; pdb.set_trace()
-        t0 = t0[internal_faces] #pegar só os internos
+        t0 = (dVtdNk*(self.component_molar_fractions * self.phase_molar_densities *\
+              mobilities).sum(axis=1)).sum(axis=0)
+
         t0 = t0 * pretransmissibility_internal_faces
 
         lines = np.array([v0[:, 0], v0[:, 1], v0[:, 0], v0[:, 1]]).flatten()
         cols = np.array([v0[:, 1], v0[:, 0], v0[:, 0], v0[:, 1]]).flatten()
         data = np.array([t0, t0, -t0, -t0]).flatten()
 
-        T = sp.csc_matrix((data, (lines, cols)), shape = (self.n_volumes, self.n_volumes))
+        T = sp.csc_matrix((data, (lines, cols)), shape = (n_volumes, n_volumes))
+
+        # diagonal matrix:
+        # see from where I get the bulk volume (sum of the fluid and rock and volumes
+        # i guess the impress already has that (it can be calculated with the mesh
+        # dimensions))
+        # diag = np.diag(V_bulk * porosity * cf - dVtdP)
+        # T += diag
         self['Tini'] = T
+
+    # def independent_terms(self):
+    #     Vp = V_bulk * porosity * (1 + cf * (P - self.P))
+    #     Q = Vp - sum(phase_moles_number/self.phase_molar_densities)
+    #
+    # def solve_pressure(self):
+    #     """pressure solver"""
+
+
+
+
+
+
         # Falta incluir o termo da derivada de V por P que soma na diagonal principal
         #e o termo da derivada de V por Nk que multiplica a transmissibilidade t0 (ele
         #entra no segundo somatório)
