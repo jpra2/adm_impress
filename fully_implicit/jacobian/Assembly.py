9aa0e3131bd3ff164c16123543b60e6a8f3cb0e3
cicero <ciceroufpe@gmail.com>
2020-02-03 15:26:28 -0300

    com Scipy

--- fully_implicit/jacobian/Assembly.py
+++ fully_implicit/jacobian/Assembly.py
@@ -1,6 +1,6 @@
 from fully_implicit.jacobian.symbolic_jacobian import symbolic_J as F_Jacobian
 import scipy
-from scipy.sparse import linalg
+from scipy.sparse import linalg, find
 import sympy as sym
 import numpy as np
 
@@ -8,6 +8,7 @@ import numpy as np
 from sympy import Heaviside as H
 
 T, S_up, Sw, So, Swn, Son, Dt, k, phi, p_i, p_j, Dx, Dy=sym.symbols("T S Sw So Swn Son Dt k phi p_i p_j Dx Dy")
+
 class Ass:
     def __init__(self,M):
         self.internal_faces=[f for f in M.all_faces if len(M.mb.get_adjacencies(f,M.dimension))==2]
@@ -17,13 +18,12 @@ class Ass:
         self.F_Jacobian=F_Jacobian().J
         # self.set_properties(M)
 
-        self.vazao=1.0
-        self.porous_volume=1*0.3  #Volume*porosidade
-        self.Dx=1.0/6
-        self.Dy=1.0/6
-        self.Dx_av=np.repeat(1.0/6,len(M.all_volumes))
-        self.Dy_av=np.repeat(1.0/6,len(M.all_volumes))
-        self.Dz_av=np.repeat(1.0/6,len(M.all_volumes))
+        self.vazao=10000.0
+
+        self.Dx=1.0
+        self.Dy=1.0
+        self.Dz=1.0
+        self.porous_volume=0.3*self.Dx*self.Dy*self.Dz  #Volume*porosidade
         self.pvi_lim=1.0
 
         for j in range(8):
@@ -33,8 +33,9 @@ class Ass:
             self.qwv=[]
             self.vpiv=[]
             self.pvi_acum=0.0
-            self.pvi=0.02
+            self.pvi=0.5
             self.dt=self.porous_volume*self.pvi/self.vazao
+            print(self.dt)
             self.set_properties(M)
             self.i=0
             self.iterac=0
@@ -52,6 +53,17 @@ class Ass:
                 self.iterac+=1
             df = pd.DataFrame (data)
             df.to_excel(filepath, index=False)
+    def apply_dirichlet(self, Mat,l):
+        ff=find(Mat)
+        lines=ff[0]
+        cols=ff[1]
+        data=ff[2]
+        l=np.unique(l)
+        for ls in l:
+            data[lines==ls]=0
+
+            data[(lines==ls) & (cols==ls)]=1
+        return scipy.sparse.csc_matrix((data, (lines,cols)),shape=Mat.shape)
 
     def Newton(self,M):
         n=len(M.all_volumes)
@@ -63,50 +75,38 @@ class Ass:
         if self.i==0:
             max_iter*=3
         i=0
+        csc_matrix=scipy.sparse.csc_matrix
+        while i<max_iter and (i==0 or max(abs(sol))>0.001):
 
-        while i<max_iter and (i==0 or max(abs(sol[n:]))>0.001):
             if i>4 or self.i>0:
                 self.pvi=self.pvi_def
                 self.dt=self.porous_volume*self.pvi/self.vazao
-            J, q=self.get_jacobian_matrix(M)
-            J[0]=0.0
-            J[0][0]=1.0
+                self.dt*=40
 
-            J[n,:]=0.0
-            J[n,n]=1.0
+            J, q=self.get_jacobian_matrix(M)
+            J2=self.apply_dirichlet(J,[0,n])
             q[n]=0.0
 
             q[0]=0.0
-            # q[-1]=q[-1].toarray()[0][0]-self.vazao
             q[-1]-=self.vazao
-            # J=scipy.sparse.csc_matrix(J)
-            Jpp=J[0:n,0:n]
-            Jps=J[0:n,n:]
-            Jsp=J[n:,0:n]
-            Jss=J[n:,n:]
 
             Fp=-q[0:n]
             Fs=-q[n:]
+            J1=scipy.sparse.csc_matrix(J2)
 
-            # J=scipy.sparse.csc_matrix(J)
-            # Jpp1=J[0:n,0:n]
-            # Jps1=J[0:n,n:]
-            # Jsp1=J[n:,0:n]
-            # Jss1=J[n:,n:]
+            Jpp1=J1[0:n,0:n]
+            Jps1=J1[0:n,n:]
+            Jsp1=J1[n:,0:n]
+            Jss1=J1[n:,n:]
 
-            # Mp1=Jpp1-Jps1*linalg.spsolve(Jss1,Jsp1)
-            Mp=Jpp-np.dot(Jps,np.linalg.solve(Jss,Jsp))
+            Mp1=Jpp1-Jps1*linalg.spsolve(Jss1,Jsp1)
 
-            # qp1=(scipy.sparse.csc_matrix(Fp).T-scipy.sparse.csc_matrix(Jps1*linalg.spsolve(Jss1,Fs)).T).toarray().transpose()[0]
-            qp=Fp-np.dot(Jps,np.linalg.solve(Jss,Fs))
+            qp1=(scipy.sparse.csc_matrix(Fp).T-scipy.sparse.csc_matrix(Jps1*linalg.spsolve(Jss1,Fs)).T).toarray().transpose()[0]
 
-            # dp1=linalg.spsolve(Mp1,qp1)
-            dp=np.linalg.solve(Mp,qp)
+            dp1=linalg.spsolve(Mp1,qp1)
+            ds1=linalg.spsolve(Jss1,Fs-Jsp1*dp1)
 
-            # ds1=linalg.spsolve(Jss1,Fs-Jsp1*dp1)
-            ds=np.linalg.solve(Jss,Fs-np.dot(Jsp,dp))
-            # import pdb; pdb.set_trace()
-            sol=np.concatenate([dp,ds])
+            sol=np.concatenate([dp1,ds1])
 
             S0+=sol[n:]
             pos_men=np.where(S0<0.2)[0]
@@ -120,7 +120,6 @@ class Ass:
             M.mb.tag_set_data(M.Swns_tag,M.all_volumes,S0[gids])
             i+=1
 
-        # Traçando curva de produção
         v0_facs=M.mb.get_adjacencies(M.all_volumes[0], M.dimension-1)
         v0_internal_faces=np.setdiff1d(v0_facs, self.boundary_faces)
         pv=M.mb.tag_get_data(M.n_pressure_tag,M.all_volumes[0],flat=True)[0]
@@ -305,7 +304,7 @@ class Ass:
         q=scipy.sparse.csc_matrix((dataq,(linesq,np.zeros(len(linesq)))),shape=(2*len(M.all_volumes),1))
 
         q=q.transpose().toarray()[0]
-        J=J.toarray()
+        # J=J.toarray()
         # np.savetxt("results/vec.csv",JJ.toarray(),delimiter=",")
 
         self.iterac+=1
--- implicit_main.py
+++ implicit_main.py
@@ -5,6 +5,7 @@ from fully_implicit.jacobian.symbolic_jacobian import symbolic_J as symbolic_J
 # M=mesh('./mesh/5x5x5.msh')
 M=MeshManager('mesh/5x5x5.msh',dim=3)
 # M=MeshManager('mesh/27x27x27.msh',dim=3)
+# M=MeshManager('mesh/10x10x10.msh',dim=3)
 # import pdb; pdb.set_trace()
 J=symbolic_J().J
 # import pdb; pdb.set_trace()
