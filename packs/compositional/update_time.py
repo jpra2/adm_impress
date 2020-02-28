af79ba00516df15b6ccee9fd7a703938a8331b5a
Eduarda <meduardagalindo8@gmail.com>
2020-02-28 15:34:20 -0300

    make the code igonre a nan error

--- packs/compositional/update_time.py
+++ packs/compositional/update_time.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class delta_time:
     def __init__(self, fprop):
         self.P = fprop.P
@@ -22,8 +23,13 @@ class delta_time:
         deltaSomax = np.nanmax(np.abs(fprop.So - self.So))
         deltaSgmax = np.nanmax(np.abs(fprop.Sg - self.Sg))
         deltaSwmax = np.nanmax(np.abs(fprop.Sw - self.Sw))
-        deltaSmax = max(deltaSomax,deltaSgmax,deltaSwmax)
-        deltaNmax = max(np.nanmax(np.abs(fprop.component_mole_numbers - self.component_mole_numbers) / fprop.component_mole_numbers, axis =1))
+        deltaSmax = max(deltaSomax, deltaSgmax, deltaSwmax)
+
+        old_settings = np.seterr(all = 'ignore', divide = 'ignore')
+        deltaNmax = max(np.nanmax(np.abs(fprop.component_mole_numbers - self.component_mole_numbers)
+                        / fprop.component_mole_numbers, axis =1))
+
+        np.seterr(**old_settings)
         deltaVmax = max(np.abs(fprop.Vt - fprop.Vp) / fprop.Vp)
 
         deltaTp = deltaT * deltaPlim / deltaPmax
