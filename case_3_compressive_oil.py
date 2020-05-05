9e06b41cd746c2b2f466e43fd7e69a9f0b83ac98
Eduarda <meduardagalindo8@gmail.com>
2020-05-05 20:31:50 -0300

    Codes for plotting test case results and comparing them to the answer

---
+++ case_3_compressive_oil.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+flying = 'flying'
+name = 'all_compositional_'
+arquivos = os.listdir(flying)
+i=1
+for arq in arquivos:
+    if  arq.startswith(name):
+        x_ans = np.array([0.997821, 0.955701, 0.981845, 0.969015, 0.936093, 0.915033, 0.897846, 0.878238, 0.858388, 0.834907, 0.801743,
+                          0.777052, 0.748971, 0.724038, 0.696926, 0.668119, 0.635924, 0.601549, 0.572259, 0.54902, 0.520697, 0.47543,
+                          0.398935, 0.29436, 0.196078, 0.136771, 0.0723796, 0, 0.033406])
+        p_ans = np.array([1986.39, 1986.04, 1986.39, 1986.39, 1985.86, 1985.63, 1985.51, 1984.93, 1984.41, 1983.54, 1982.55, 1981.62,
+                          1980.22, 1979.12, 1977.66, 1975.97, 1973.94, 1971.55, 1969.17, 1967.25, 1965.04, 1960.79, 1952.94, 1940.9,
+                          1928.27, 1920.54, 1911.46, 1900, 1905.88])
+
+
+        datas = np.load('flying/all_compositional_b_results_820.npy', allow_pickle=True)
+
+        for data in datas[2:]:
+            pressure = data[4] / 6894.757
+            #pressure = (data[4] - 13.78951458E6 * np.ones(100))/6894.76
+            time = data[3]
+            loop = data[0]
+            #flux = data[5]
+            #flux_vector = data[6]
+            i=loop
+            #flux_vols = data[5]
+            x = np.linspace(0,1,100)
+        #    p_resp = np.linspace(0.623843,0,100)
+            plt.figure(1)
+            plt.plot(x, pressure, 'r', x_ans, p_ans, 'y')
+            plt.grid()
+            plt.legend(('PADMEC', 'Benchmark'))
+            plt.ylabel('Pressure (psi)')
+            plt.xlabel('Dimensionless distance')
+            plt.savefig('results/compositional/pressure_comp_oil' + str(loop) + '.png')
+
+            import pdb; pdb.set_trace()
+
+
+'''
+        p1 = (datas[1][4] - 13.78951458E6 * np.ones(100))/6894.76
+        p2 = (datas[2][4] - 13.78951458E6 * np.ones(100))/6894.76
+        p3 = (datas[3][4] - 13.78951458E6 * np.ones(100))/6894.76
+        p4 = (datas[8][4] - 13.78951458E6 * np.ones(100))/6894.76
+
+        t1 = datas[1][3]
+        t2 = datas[2][3]
+        t3 = datas[3][3]
+        t4 = datas[8][3]
+        x = np.linspace(0,1,100)
+        plt.figure(1)
+        plt.plot(x, p1, 'r', x, p2, 'b', x, p3, 'g', x, p4, 'y')
+        plt.grid()
+        plt.legend(('%f seg' %t1, '%f seg' %t2, '%f seg' %t3, 'solução'))
+        plt.ylabel('Pressure Drop (psi)')
+        plt.xlabel('Dimensionless distance')
+        plt.savefig('results/compositional/pressure_mcN_more3' + '.png')
+        import pdb; pdb.set_trace()'''
---
+++ case_9_Buckley_Leverett.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+flying = 'flying'
+name = 'all_compositional_'
+arquivos = os.listdir(flying)
+i=1
+
+krw_end = 0.2
+kro_end = 1
+nw = 2
+no = 2
+Swr = 0.2
+Sor = 0.35
+mi_w = 1e-3
+mi_o = 20e-3
+Sw = np.linspace(Swr, 1-Sor, 500)
+Sw1 = np.zeros([1,1])
+Sw3 = np.zeros([1,1])
+xD3 = np.zeros([1,1])
+td = 0.2
+
+S = (Sw - Swr)/(1 - Swr - Sor)
+krw = krw_end*S**nw
+kro = kro_end*(1-S)**no
+fw = 1/ (1+(kro*mi_w)/(krw*mi_o))
+
+p1 = np.polyfit(Sw, fw, 12)
+p2 = np.polyder(p1)
+diff_fw = np.polyval(p2, Sw)
+
+## Find the saturation shock:
+for n in range(0, 500):
+    if abs((fw[n])/(Sw[n]-Swr) - diff_fw[n]) < 0.009:
+        Swf = Sw[n]
+        slope_Swf = (fw[n])/(Sw[n]-Swr)
+        a = n
+
+##Water saturation profile of water front:
+Sw2 = np.linspace(Swr, Swf, 5)
+xD2 = np.linspace(slope_Swf*td,slope_Swf*td,5)
+
+## Water saturation profile before water front:
+Sw1 = np.zeros(a-1)
+for i in range(0,a-1):
+    Sw1[i] = Swr
+    xD1 = np.linspace(1, slope_Swf*td,a-1)
+
+## Water saturation profile behind water front:
+Sw3 = np.zeros(500-a+1)
+xD3 = np.zeros(500-a+1)
+for j in range(a-1,500):
+    Sw3[j-(a-1)] = Sw[j]
+    xD3[j-(a-1)] = diff_fw[j]*td
+
+xD = np.append(xD1,xD2)
+xD = np.append(xD,xD3)
+SwD = np.append(Sw1,Sw2)
+SwD = np.append(SwD,Sw3)
+
+
+
+for  arq in arquivos:
+    if  arq.startswith(name):
+        datas = np.load('flying/all_compositional_BL_results.npy', allow_pickle=True)
+        import pdb; pdb.set_trace()
+        for data in datas[199:]:
+            Sw = data[5]
+            x = np.linspace(0,1,500)
+            plt.figure(1)
+            plt.plot(x, Sw, 'r', xD, SwD, 'y')
+            plt.grid()
+            loop = data[0]
+            plt.legend(('PADMEC', 'Analytical Solution'))
+            plt.title('Buckley-Leverett Solution Example')
+            plt.ylabel('Water Saturation')
+            plt.xlabel('Dimensionless distance')
+            plt.savefig('results/compositional/saturation_w_BL_comparison.png')
+            import pdb; pdb.set_trace()
