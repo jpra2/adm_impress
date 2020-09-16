afae40a22fc5be2fc7a1c56916b68f100b4dbdca
Filipe Cumaru <facsa@cin.ufpe.br>
2020-09-16 16:33:16 -0300

    Add docstrings for the VugGenerator class methods.

--- packs/vugs_generator/vugs_generator.py
+++ packs/vugs_generator/vugs_generator.py
@@ -3,11 +3,35 @@ from .impress.preprocessor.meshHandle.finescaleMesh import FineScaleMesh
 
 class VugGenerator(object):
     def __init__(self, mesh_file, ellipsis_params_range, num_ellipsoids=10):
+        """
+        Constructor method.
+
+        Inputs
+        ------
+        mesh_file (String): A string containing the path to the input mesh file.
+        ellipsis_params_range (iterable of size 2): A iterable containing the maximum and the
+        minimum value for the ellipsoids parameters.
+        num_ellipsoids (int): the number of ellipsoids to create.
+
+        """
         self.mesh = FineScaleMesh(mesh_file)
         self.ellipsis_params_range = ellipsis_params_range
         self.num_ellipsoids = num_ellipsoids
 
     def run(self):
+        """
+        Main method. Creates random sized and rotated ellipsoids and
+        assigns the volumes inside the vugs.
+        
+        Inputs
+        ------
+        None
+
+        Output
+        ------
+        None
+
+        """
         centroids = self.mesh.volumes.center[:]
         xs, ys, zs = centroids[:, 0], centroids[:, 1], centroids[:, 2]
         x_range = xs.min(), xs.max()
@@ -24,11 +48,46 @@ class VugGenerator(object):
             self.mesh.vug[vols_in_vug < 1] = 1
 
     def write_file(self, path="results/vugs.vtk"):
+        """
+        Writes the resultant mesh into a file. Default path is 'results/vugs.vtk'.
+
+        Inputs
+        ------
+        path (String): A string containing the output file name.
+
+        Outputs
+        -------
+        None
+        
+        """
         vugs_meshset = self.mesh.core.mb.create_meshset()
         self.mesh.core.mb.add_entities(vugs_meshset, self.mesh.core.all_volumes)
         self.mesh.core.mb.write_file(path, [vugs_meshset])
     
     def get_random_ellipsoids(self, x_range, y_range, z_range):
+        """
+        Generates random points as the ellipsoids centers as the axis sizes 
+        and random rotation angles with respect to the cartesian coordinates (x,y,z).
+
+        Inputs
+        ------
+        x_range (iterable of size 2): A iterable containing the maximum and minimum 
+        values of the x coordinate.
+        y_range (iterable of size 2): A iterable containing the maximum and minimum 
+        values of the y coordinate.
+        z_range (iterable of size 2): A iterable containing the maximum and minimum 
+        values of the z coordinate.
+
+        Outputs
+        -------
+        random_centers (num_ellipsoids x 3 numpy array): The generated center 
+        points for the ellipsoids.
+        random_params (num_ellipsoids x 3 numpy array): The parameters a.k.a 
+        the size of the three axis of the ellipsoids.
+        random_angles (num_ellipsoids x 3 numpy array): The rotation angles 
+        for each ellipsoid.
+        
+        """
         rng = np.random.default_rng()
         random_centers = np.zeros((self.num_ellipsoids, 3))
 
@@ -43,12 +102,27 @@ class VugGenerator(object):
         return random_centers, random_params, random_angles
     
     def get_rotation_matrix(self, angle):
+        """
+        Calculates the rotation matrix for the given angle. Such matrix is the result of 
+        three rotations: first with respect to x, then y, then z.
+
+        Inputs
+        ------
+        angle (3 x 1 numpy array): The three rotation angles in radians.
+        
+        Outputs
+        -------
+        R (3 x 3 numpy array): The rotation matrix.
+
+        """
         cos_ang = np.cos(angle)
         sin_ang = np.sin(angle)
-        return np.array([
+        R = np.array([
             cos_ang[1]*cos_ang[2], -cos_ang[1]*sin_ang[2], sin_ang[1], \
             sin_ang[0]*sin_ang[1]*cos_ang[2] + cos_ang[0]*sin_ang[2], \
             cos_ang[0]*cos_ang[2] - sin_ang[0]*sin_ang[1]*sin_ang[2], \
             -sin_ang[0]*sin_ang[1], sin_ang[0]*sin_ang[2] - cos_ang[0]*sin_ang[1]*sin_ang[2], \
             cos_ang[0]*sin_ang[1]*sin_ang[2] + sin_ang[0]*cos_ang[2], cos_ang[0]*cos_ang[1]
         ]).reshape((3,3))
+
+        return R
