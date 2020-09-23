fc808eab77297eea92eb6fff8cda730e195dd29d
jp <jprandrade2@gmail.com>
2020-09-23 12:19:50 -0300

    test cf with injector

--- ADM_b2.py
+++ ADM_b2.py
@@ -400,31 +400,37 @@ volumes_without_grav_level_0 = ml_data['volumes_without_grav_level_0']
 data_impress['verif_rest'][:] = 0.0
 data_impress['verif_rest'][volumes_without_grav_level_0] = 1.0
 
-t0=time.time()
-b2 = g_source_total_volumes.copy()
+def get_cfs(g_source_total_volumes, volumes_without_grav_level_0, wells2, data_impress, local_lu_matrices, As):
 
-# b2 = b.copy()
-# b2[wells2['ws_p']] = 0.0
-# b2[wells[]]
-b2[volumes_without_grav_level_0] = 0
-# b2[data_impress['LEVEL']==0] = 0.0
+    t0=time.time()
+    b2 = g_source_total_volumes.copy()
+    b2[volumes_without_grav_level_0] = 0
+    b2[wells2['ws_q']] += wells2['values_q']
 
-# b2[data_impress['DUAL_1'] == 2] = 0.0
+    # b2 = b.copy()
+    # b2[wells2['ws_p']] = 0.0
+    # b2[wells[]]
+    # b2[data_impress['LEVEL']==0] = 0.0
 
-# b2=np.ones_like(b2)10000
-# b2[wells['ws_q']] = -b[wells['ws_q']]
-# b2[wells['ws_p']] = 0
-# cfs = get_correction_function(local_lu_matrices.local_lu_and_global_ids, As, np.ones_like(b2))
-cfs = get_correction_function(local_lu_matrices.local_lu_and_global_ids, As, b2)
-cfs[data_impress['LEVEL']==0] = 0.0
-# cfs[data_impress['LEVEL'] == 0] = 0.0
-# cfs = get_correction_function(local_lu_matrices.local_lu_and_global_ids, As, np.zeros_like(b2))
-# cfs[wells['ws_p']] = 0
-# cfs[:] = 0
-data_impress['gama']=cfs
-print('cf {}'.format(time.time()-t0))
+    # b2[data_impress['DUAL_1'] == 2] = 0.0
+
+    # b2=np.ones_like(b2)10000
+    # b2[wells['ws_q']] = -b[wells['ws_q']]
+    # b2[wells['ws_p']] = 0
+    # cfs = get_correction_function(local_lu_matrices.local_lu_and_global_ids, As, np.ones_like(b2))
+    cfs = get_correction_function(local_lu_matrices.local_lu_and_global_ids, As, b2)
+    cfs[data_impress['LEVEL']==0] = 0.0
+    # cfs[data_impress['LEVEL'] == 0] = 0.0
+    # cfs = get_correction_function(local_lu_matrices.local_lu_and_global_ids, As, np.zeros_like(b2))
+    # cfs[wells['ws_p']] = 0
+    # cfs[:] = 0
+    data_impress['gama']=cfs
+    print('cf {}'.format(time.time()-t0))
+    return cfs
 # import pdb; pdb.set_trace()
 #############################
+
+cfs = get_cfs(g_source_total_volumes, volumes_without_grav_level_0, wells2, data_impress, local_lu_matrices, As)
 
 # mlo=tpfalize(M,mlo,data_impress)
 mlo['prolongation_lcd_level_1']=sp.find(mlo['prolongation_level_1'])
@@ -1034,14 +1040,16 @@ while verif:
 
     ###############
 
-    b2 = g_source_total_volumes.copy()
-    # b2[wells2['ws_p']] = 0.0
-    b2[volumes_without_grav_level_0] = 0.0
-    # b2[data_impress['LEVEL'] == 0] = 0.0
-    # b2[data_impress['LEVEL'] == 0] = 0.0
-    cfs = get_correction_function(local_lu_matrices.local_lu_and_global_ids, As, b2)
-    # cfs[data_impress['LEVEL'] == 0] = 0.0
-    data_impress['gama'][:]=cfs
+    # b2 = g_source_total_volumes.copy()
+    # # b2[wells2['ws_p']] = 0.0
+    # b2[volumes_without_grav_level_0] = 0.0
+    # # b2[data_impress['LEVEL'] == 0] = 0.0
+    # # b2[data_impress['LEVEL'] == 0] = 0.0
+    # cfs = get_correction_function(local_lu_matrices.local_lu_and_global_ids, As, b2)
+    # # cfs[data_impress['LEVEL'] == 0] = 0.0
+    # data_impress['gama'][:]=cfs
+
+    cfs = get_cfs(g_source_total_volumes, volumes_without_grav_level_0, wells2, data_impress, local_lu_matrices, As)
 
     OP_ADM = adm_method['adm_prolongation_level_1']
     OR_ADM = adm_method['adm_restriction_level_1']
@@ -1172,6 +1180,7 @@ while verif:
         elements.volumes,
         flux_total_volumes
     )
+
     data_impress['flux_w_volumes'][:] = flux_w_volumes
 
     flux_o_volumes = biphasic.get_flux_phase_volumes(
