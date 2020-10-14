45afea49c89e19e97598dd629677037e7f7191ea
JP <jprandrade2@gmail.com>
2020-10-14 10:30:33 -0300

    test1 front refinement

--- packs/adm/adm_method.py
+++ packs/adm/adm_method.py
@@ -130,6 +130,7 @@ class AdmMethod(DataManager, TpfaFlux2):
         self.ml_data = M.multilevel_data
         self.all_wells_ids = all_wells_ids
         self.n_levels = n_levels
+
         # self.delta_sat_max = 0.05
         # self.delta_sat_max = 2.0
         # self.delta_sat_max = np.load('flying/delta_sat_max.npy')[0]
