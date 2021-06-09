9b54a68154113da55d396d05fad95efc333f6121
joao <jprandrade2@gmail.com>
2021-06-09 13:34:38 -0300

    update volume, gravity and capillary terms

--- packs/compositional/IMPEC/global_pressure_solver.py
+++ packs/compositional/IMPEC/global_pressure_solver.py
@@ -52,10 +52,12 @@ class GlobalIMPECPressureSolver:
         return T.tocsc()
 
     @staticmethod
-    def mount_independent_term(Vbulk, porosity, Cf, dVtdP, P, n_volumes, n_components, n_phases, internal_faces_adjacencies, dVtdk, z_centroids, xkj_internal_faces, Csi_j_internal_faces, mobilities_internal_faces, pretransmissibility_internal_faces, Pcap):
+    def mount_independent_term(Vbulk, porosity, Cf, dVtdP, P, n_volumes, n_components, n_phases, internal_faces_adjacencies, dVtdk, z_centroids, xkj_internal_faces, Csi_j_internal_faces, mobilities_internal_faces, pretransmissibility_internal_faces, Pcap, Vp, Vt):
         pressure_term = GlobalIMPECPressureSolver.pressure_independent_term(Vbulk, porosity, Cf, dVtdP, P)
         gravity_term = GlobalIMPECPressureSolver.gravity_independent_term(n_volumes, n_components, internal_faces_adjacencies, dVtdk, z_centroids)
         capillary_term = GlobalIMPECPressureSolver.capillary_independent_term(xkj_internal_faces, Csi_j_internal_faces, mobilities_internal_faces, pretransmissibility_internal_faces, n_components, n_phases, n_volumes, dVtdk, Pcap)
+        volume_term = GlobalIMPECPressureSolver.volume_discrepancy_independent_term(Vp, Vt)
+
 
     def update_independent_terms(self, M, fprop, wells, delta_t):
         self.pressure_term = self.pressure_independent_term(fprop)
@@ -83,9 +85,15 @@ class GlobalIMPECPressureSolver:
         return pressure_term
 
     @staticmethod
-    def gravity_independent_term(n_volumes, n_components, internal_faces_adjacencies, dVtdk, z_centroids):
+    def gravity_independent_term(xkj_internal_faces, Csi_j_internal_faces, mobilities_internal_faces, rho_j_internal_faces, pretransmissibility_internal_faces, n_volumes, n_components, internal_faces_adjacencies, dVtdk, z_centroids, g):
         """
 
+        @param g: gravity acc in z direction (scalar)
+        @param xkj_internal_faces: concentration of component in phase (n_components, n_phases, n_internal_faces)
+        @param Csi_j_internal_faces: molar densities of phases (1, n_phases, n_internal_faces)
+        @param mobilities_internal_faces: (1, n_phases, n_internal_faces)
+        @param rho_j_internal_faces: density of phase in internal faces (1, n_phases, n_internal_faces)
+        @param pretransmissibility_internal_faces: static params of face transmissibility
         @param n_volumes: number of volumes
         @param n_components: number of components
         @param internal_faces_adjacencies: volumes adjacencies of internal faces
@@ -95,6 +103,9 @@ class GlobalIMPECPressureSolver:
         """
 
         v0 = internal_faces_adjacencies
+        t0_internal_faces_prod = xkj_internal_faces * Csi_j_internal_faces * mobilities_internal_faces
+        t0_j = t0_internal_faces_prod * pretransmissibility_internal_faces
+        t0_k = g * np.sum(rho_j_internal_faces * t0_j, axis=1)
 
         grav = np.zeros([n_volumes, n_volumes])
 
@@ -109,7 +120,7 @@ class GlobalIMPECPressureSolver:
         return gravity_term
 
     @staticmethod
-    def capillary_independent_term(xkj_internal_faces, Csi_j_internal_faces, mobilities_internal_faces, pretransmissibility_internal_faces, n_components, n_phases, n_volumes, dVtdk, Pcap):
+    def capillary_independent_term(xkj_internal_faces, Csi_j_internal_faces, mobilities_internal_faces, pretransmissibility_internal_faces, n_components, n_phases, n_volumes, dVtdk, Pcap, internal_faces_adjacencies):
         """
 
         @param xkj_internal_faces: concentration of component in phase (n_components, n_phases, n_internal_faces)
@@ -121,11 +132,13 @@ class GlobalIMPECPressureSolver:
         @param n_volumes: number of volumes
         @param dVtdk: volume derivatives with respect to component mol number (n_components, n_volumes)
         @param Pcap: capipllary pressure of phases (n_phases, n_volumes)
+        @param internal_faces_adjacencies: volumes adjacencies of internal faces
         @return: capillary independent term
         """
 
         t0_internal_faces_prod = xkj_internal_faces * Csi_j_internal_faces * mobilities_internal_faces
         t0_j = t0_internal_faces_prod * pretransmissibility_internal_faces
+        v0 = internal_faces_adjacencies
 
         cap = np.zeros([n_volumes])
         for i in range(n_components):
@@ -137,4 +150,16 @@ class GlobalIMPECPressureSolver:
                 cap += t0 @ Pcap[j, :]
 
         return cap
+
+    @staticmethod
+    def volume_discrepancy_independent_term(Vp, Vt):
+        """
+
+        @param Vp: porous volume (n_volumes)
+        @param Vt: fluid volume (n_volumes)
+        @return: volume discrepancy term (n_volumes)
+        """
+        volume_discrepancy_term = Vp - Vt
+        return volume_discrepancy_term
+
 
