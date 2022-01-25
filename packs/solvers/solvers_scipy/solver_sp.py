42eefd23b1ec56716e401fcdab4115a70a37236c
joao <jprandrade2@gmail.com>
2022-01-25 10:33:31 -0300

    up

--- packs/solvers/solvers_scipy/solver_sp.py
+++ packs/solvers/solvers_scipy/solver_sp.py
@@ -1,5 +1,25 @@
 import scipy.sparse as sp
 from scipy.sparse import linalg
+import numpy as np
+
+
+norm_rk = []
+niter = 0
+
+class ScipyGmresCounter(object):
+    def __init__(self, disp=True):
+        self._disp = disp
+        global norm_rk
+        global niter
+        norm_rk = []
+        niter = 0
+    def __call__(self, rk=None):
+        global norm_rk
+        global niter
+        niter += 1
+        norm_rk.append(np.linalg.norm(rk))
+        # if self._disp:
+        #     print('iter %3i\trk = %s' % (self.niter, str(rk)))
 
 
 class SolverSp:
@@ -31,6 +51,7 @@ class SolverSp:
     def gmres_solver(self, A, b, x0=None, tol=1e-5, precond=None):
 
         print('\nSolving gmres solver\n')
+        counter_callback = ScipyGmresCounter(disp=False)
 
         n = A.shape[0]
         if precond:
@@ -42,7 +63,9 @@ class SolverSp:
         else:
             M = None
 
-        x, exitcode = linalg.gmres(A, b, x0=x0, tol=tol, M=M)
+        # x, exitcode = linalg.gmres(A, b, x0=x0, tol=tol, M=M)
+        x, exitcode = linalg.gmres(A, b, x0=x0, tol=tol, M=M, callback=counter_callback)
+        ## exitcode = 0: indicates successful convergence
 
         return x
 
