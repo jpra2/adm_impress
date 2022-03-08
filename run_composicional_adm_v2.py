9cc969ca3018ead93674f4f2a7e87933c07e8efd
joao <jprandrade2@gmail.com>
2022-03-08 14:31:11 -0300

    update 1D 6k adm

--- run_composicional_adm_v2.py
+++ run_composicional_adm_v2.py
@@ -1,6 +1,6 @@
-# from packs.cases.compositional_adm_cases.compressible_oil import case2_adm
+from packs.cases.compositional_adm_cases.compressible_oil import case2_adm
 # from packs.cases.compositional_adm_cases.compressible_oil import case1_finescale
-from packs.cases.compositional_adm_cases.compressible_oil import compare_solution
+# from packs.cases.compositional_adm_cases.compressible_oil import compare_solution
 
 
 import pdb; pdb.set_trace()
