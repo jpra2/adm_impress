4608f8dbb5b18364ac01d06e43f14c2ffb4cbe04
JP <jprandrade2@gmail.com>
2022-03-16 14:56:14 -0300

    up

--- run_composicional_adm_v2.py
+++ run_composicional_adm_v2.py
@@ -1,5 +1,5 @@
-# from packs.cases.compositional_adm_cases.compressible_oil import case2_adm
-from packs.cases.compositional_adm_cases.compressible_oil import case1_finescale
+from packs.cases.compositional_adm_cases.compressible_oil import case2_adm
+# from packs.cases.compositional_adm_cases.compressible_oil import case1_finescale
 # from packs.cases.compositional_adm_cases.compressible_oil import compare_solution
 
 
