b736d406b43141b0cb78b84d24ccbf3520ef1c23
Eduarda <meduardagalindo8@gmail.com>
2022-06-03 00:46:19 -0300

    commented some stuff

--- packs/compositional/IMPEC/properties_calculation.py
+++ packs/compositional/IMPEC/properties_calculation.py
@@ -198,8 +198,8 @@ class PropertiesCalc:
             #phase_viscosities[0,0:2,:] = 0.000249*np.ones([2,len(Csi_j[0,0,:])]) #0.02 only for BL test. for BL_Darlan use 1e-3
             #phase_viscosities[0,0:2,:] = 0.001*np.ones([2,len(Csi_j[0,0,:])]) #only for Dietz test; 0.000249 for 2D injec Li
             phase_viscosities[0,0:2,:] = phase_viscosity(fprop, xkj)
-            phase_viscosities[0,0,:] = 0.01
-            phase_viscosities[0,1,:] = 0.001#1/5*phase_viscosities[0,0,:] #for 5k NVCM case mug=muo; for 3k Orr NVCM mug=1/5muo
+            #phase_viscosities[0,0,:] = 0.01
+            #phase_viscosities[0,1,:] = 0.001#1/5*phase_viscosities[0,0,:] #for 5k NVCM case mug=muo; for 3k Orr NVCM mug=1/5muo
         if ctes.load_w:
             phase_viscosities[0,ctes.n_phases-1,:] = data_loaded['compositional_data']['water_data']['mi_W']
         return phase_viscosities
