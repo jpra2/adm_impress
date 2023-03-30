9679020952c8e140eebe758292e6480f160d196d
jpra2 <jprandrade2@gmail.com>
2023-03-30 11:57:26 -0300

    internal nodes weights

--- packs/mpfa_methods/weight_interpolation/gls_weight_2d.py
+++ packs/mpfa_methods/weight_interpolation/gls_weight_2d.py
@@ -5,6 +5,11 @@ class CalculateGlsWeight2D:
 
     def __init__(self, **kwargs):
         """ Initialize the class
+            
+            paper: A least squares based diamond scheme for anisotropic
+            diffusion problems on polygonal meshes
+            
+            doi: 10.1002/fld.5031
 
         @param kwargs: dict with the keys:
             adjacencies: faces adjacencies of edges,
@@ -41,115 +46,145 @@ class CalculateGlsWeight2D:
 
     def get_weights_internal_nodes(self):
         
+        dtype = [('node_id', int), ('face_id', int), ('weight', float)]
+        
+        nodes_ids = []
+        faces_ids = []
+        all_weight = []
+
+        weights = []
         for node in self.nodes[self.bool_internal_nodes]:
             nodes_adj = self.nodes_of_nodes[node]
             edges_adj = self.edges_of_nodes[node]
             centroid_node = self.nodes_centroids[node]
             centroids_nodes_adj = self.nodes_centroids[nodes_adj]
             faces_adj = self.faces_of_nodes[node]
-            adj_edges_adj = self.adjacencies[edges_adj]
             centroids_faces_adj = self.faces_centroids[faces_adj]
+
+            n = len(faces_adj)
+            # mfaces = self.mfaces(n, centroids_faces_adj, centroid_node)
+            # mnodes = self.mnodes(len(nodes_adj), centroids_nodes_adj, centroid_node)
+            # mnormalperm = self.mnormalperm(n, faces_adj, edges_adj, self.adjacencies, self.unitary_normal_edges, self.permeability)
+            # Mv = self.Mv(n, mfaces, mnodes, mnormalperm)
+            # Nv = self.Nv(n)            
+            # M = self.M(Mv, Nv)
+
+            M = self.M(
+                self.Mv(
+                    n,
+                    self.mfaces(n, centroids_faces_adj, centroid_node),
+                    self.mnodes(len(nodes_adj), centroids_nodes_adj, centroid_node),
+                    self.mnormalperm(n, faces_adj, edges_adj, self.adjacencies, self.unitary_normal_edges, self.permeability)
+                ),
+                self.Nv(n)
+            )
+
             
-            #################################################
-            n_mfaces = len(faces_adj)
-            mfaces = np.zeros((n_mfaces, 2*n_mfaces))
-            f_dists = centroids_faces_adj - centroid_node
-            for i in range(n_mfaces):
-                mfaces[i,2*i:2*i+2] = f_dists[i]
-            #################################################
+            weights = self.eT(n).dot(M)
+
+            nodes_ids.append(np.repeat(node, n))
+            faces_ids.append(faces_adj)
+            all_weight.append(weights)
+        
+        nodes_ids = np.concatenate(nodes_ids)
+        faces_ids = np.concatenate(faces_ids)
+        all_weight = np.concatenate(all_weight)
+
+        return nodes_ids, faces_ids, all_weight
+
+    def eT(self, n):
+        eT = np.zeros(2*n+1)
+        eT[-1] = 1
+        return eT
+
+    def mfaces(self, n_mfaces: int, centroids_faces_adj: np.ndarray, centroid_node: np.ndarray):
+        mfaces = np.zeros((n_mfaces, 2*n_mfaces))
+        f_dists = centroids_faces_adj - centroid_node
+        for i in range(n_mfaces):
+            mfaces[i,2*i:2*i+2] = f_dists[i]
+        
+        return mfaces
+
+    def mnodes(self, n_mnodes, centroids_nodes_adj, centroid_node):
+        
+        mnodes = np.zeros((n_mnodes, 2*n_mnodes))
+        nodes_dists = centroids_nodes_adj - centroid_node
+        for i in range(n_mnodes):
+            mnodes[i,2*i:2*i+2] = nodes_dists[i]
             
-            #################################################
-            n_mnodes = len(nodes_adj)
-            mnodes = np.zeros((n_mnodes, 2*n_mnodes))
-            nodes_dists = centroids_nodes_adj - centroid_node
-            for i in range(n_mnodes):
-                mnodes[i,2*i:2*i+2] = nodes_dists[i]
-                
-            mnodes[0, 2*n_mnodes-2:] = -nodes_dists[0]
-            
-            for i in range(1, n_mnodes):
-                mnodes[i, 2*i-2:2*i] = -nodes_dists[i]
-            #################################################
-            
-            ##################################################
-            n_mnormalperm = n_mfaces
-            mnormalperm = np.zeros((n_mnormalperm, 2*n_mnormalperm))
-            for i in range(n_mnormalperm):
-                face = faces_adj[i]
-                edge = edges_adj[i]
-                faces_adj_edge = self.adjacencies[edge]
-                unitary_normal_edge = self.unitary_normal_edges[edge]
-                face_perm = self.permeability[face]
-                if face == faces_adj_edge[1]:
-                    normal = -unitary_normal_edge
-                else:
-                    normal = unitary_normal_edge
-                
-                mnormalperm[i, 2*i:2*i+2] = np.dot(normal, face_perm)
-            
-            for i in range(1, n_mnormalperm):
-                face = faces_adj[i]
-                edge = edges_adj[i]
-                faces_adj_edge = self.adjacencies[edge]
-                unitary_normal_edge = self.unitary_normal_edges[edge]
-                face_perm = self.permeability[faces_adj_edge[faces_adj_edge!=face][0]]
-                if face == faces_adj_edge[1]:
-                    normal = -unitary_normal_edge
-                else:
-                    normal = unitary_normal_edge
-                
-                mnormalperm[i, 2*i-2:2*i] = -np.dot(normal, face_perm)
-            
-            i = 0
+        mnodes[0, 2*n_mnodes-2:] = -nodes_dists[0]
+        
+        for i in range(1, n_mnodes):
+            mnodes[i, 2*i-2:2*i] = -nodes_dists[i]
+        
+        return mnodes
+
+    def mnormalperm(self, n_mfaces, faces_adj, edges_adj, adjacencies, unitary_normal_edges, permeability):
+
+        n_mnormalperm = n_mfaces
+        mnormalperm = np.zeros((n_mnormalperm, 2*n_mnormalperm))
+        for i in range(n_mnormalperm):
             face = faces_adj[i]
             edge = edges_adj[i]
-            faces_adj_edge = self.adjacencies[edge]
-            unitary_normal_edge = self.unitary_normal_edges[edge]
-            face_perm = self.permeability[faces_adj_edge[faces_adj_edge!=face][0]]
+            faces_adj_edge = adjacencies[edge]
+            unitary_normal_edge = unitary_normal_edges[edge]
+            face_perm = permeability[face]
             if face == faces_adj_edge[1]:
                 normal = -unitary_normal_edge
             else:
                 normal = unitary_normal_edge
             
-            mnormalperm[0, 2*n_mnormalperm-2:] = -np.dot(normal, face_perm)
-            ########################################################
-
-            ###################################################
-            Mv = np.zeros((3*n_mfaces, 2*n_mfaces+1))
-            Mv[0:n_mfaces,-1] = 1
-            Mv[0:n_mfaces, 0:2*n_mfaces] = mfaces
-            Mv[n_mfaces:2*n_mfaces, 0:2*n_mfaces] = mnodes
-            Mv[2*n_mfaces:3*n_mfaces, 0:2*n_mfaces] = mnormalperm
-            ###################################################
-
-            #####################################
-            Nv = np.zeros((3*n_mfaces, n_mfaces))
-            Nv[np.arange(n_mfaces), np.arange(n_mfaces)] = 1
-            #####################################
-
-            ##################################
-            V = np.linalg.inv(np.dot(Mv, Mv.T))
-            ##################################
-
-
-
+            mnormalperm[i, 2*i:2*i+2] = np.dot(normal, face_perm)
+        
+        for i in range(1, n_mnormalperm):
+            face = faces_adj[i]
+            edge = edges_adj[i]
+            faces_adj_edge = adjacencies[edge]
+            unitary_normal_edge = unitary_normal_edges[edge]
+            face_perm = permeability[faces_adj_edge[faces_adj_edge!=face][0]]
+            if face == faces_adj_edge[1]:
+                normal = -unitary_normal_edge
+            else:
+                normal = unitary_normal_edge
             
-            import pdb; pdb.set_trace()
+            mnormalperm[i, 2*i-2:2*i] = -np.dot(normal, face_perm)
+        
+        i = 0
+        face = faces_adj[i]
+        edge = edges_adj[i]
+        faces_adj_edge = adjacencies[edge]
+        unitary_normal_edge = unitary_normal_edges[edge]
+        face_perm = permeability[faces_adj_edge[faces_adj_edge!=face][0]]
+        if face == faces_adj_edge[1]:
+            normal = -unitary_normal_edge
+        else:
+            normal = unitary_normal_edge
+        
+        mnormalperm[0, 2*n_mnormalperm-2:] = -np.dot(normal, face_perm)
 
+        return mnormalperm
 
+    def Mv(self, n, mfaces, mnodes, mnormalperm):
+        
+        n_mfaces = n
+        Mv = np.zeros((3*n_mfaces, 2*n_mfaces+1))
+        Mv[0:n_mfaces,-1] = 1
+        Mv[0:n_mfaces, 0:2*n_mfaces] = mfaces
+        Mv[n_mfaces:2*n_mfaces, 0:2*n_mfaces] = mnodes
+        Mv[2*n_mfaces:3*n_mfaces, 0:2*n_mfaces] = mnormalperm
+        
+        return Mv
 
-                
+    def Nv(self, n):
+        
+        n_mfaces = n
+        Nv = np.zeros((3*n_mfaces, n_mfaces))
+        Nv[np.arange(n_mfaces), np.arange(n_mfaces)] = 1
 
+        return Nv
 
-            
-                
-            
-            import pdb; pdb.set_trace()
-
-
-
-
-
-
-
-
+    def M(self, Mv, Nv):
+        return np.linalg.inv(Mv.T.dot(Mv)).dot(Mv.T).dot(Nv)
+    
+    def weights(self, eT, M):
+        return eT.dot(M)
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 # from packs.tests import test_dataset
 # from packs.tests import test_geometric_data
 # from packs.tests import test_preprocess_tpfa
-from packs.tests import test_biphasic_tpfa_cons
+# from packs.tests import test_biphasic_tpfa_cons
 # from packs.tests import test_2_generate_non_uniform_mesh
 # from packs.tests import test_3_prolongation_with_pcorr
 # from packs.tests import test_4_monophasic
+from packs.examples import mpfad_gls_test
