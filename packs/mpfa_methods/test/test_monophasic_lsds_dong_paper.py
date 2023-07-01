177de2b4d56d6aeb9d7784177952c0f89a0a0c9b
jpra2 <jprandrade2@gmail.com>
2023-07-01 16:45:09 -0300

    update

--- packs/mpfa_methods/test/test_monophasic_lsds_dong_paper.py
+++ packs/mpfa_methods/test/test_monophasic_lsds_dong_paper.py
@@ -9,6 +9,9 @@ from scipy.sparse.linalg import spsolve
 import os
 from packs.manager.mesh_data import MeshData
 import matplotlib.pyplot as plt
+from packs.utils import calculate_face_properties
+
+all_pr_names = ['problem1', 'problem2', 'problem3']
 
 def exact_solution_p1(centroids):
     x = centroids[:, 0]
@@ -17,7 +20,62 @@ def exact_solution_p1(centroids):
     term2 = np.power(1-x, 3)*np.power(1-y, 2)
     return 0.5*(term1 + term2)
 
-def get_permeability_p1(n_elements):
+def exact_solution_p2(centroids):
+    x = centroids[:, 0]
+    y = centroids[:, 1]
+    resp = 16*x*(1-y)*y*(1-y)
+    return resp
+    
+def exact_solution_p3(centroids):
+    x = centroids[:, 0]
+    y = centroids[:, 1]
+    
+    solution = np.zeros(len(x))
+    
+    t1 = x <= 0.5
+    t2 = x > 0.5
+    
+    x1 = x[t1]
+    y1 = y[t1]
+    
+    x2 = x[t2]
+    y2 = y[t2]
+    
+    solution[t1] = 1 - 2*np.power(y1, 2) + 4*x1*y1 + 6*x1 + 2*y1
+    solution[t2] = -2*np.power(y2, 2) + 1.6*x2*y2 -0.6*x2 + 3.2*y2 + 4.3
+    
+    return solution
+    
+    
+    
+    
+
+def get_permeability_p3(n_elements, centroids):
+    x = centroids[:, 0]
+    permeability = np.zeros((n_elements, 2, 2))
+    
+    k1 = np.array([
+        [1, 0],
+        [0, 1]
+    ])
+    
+    k2 = np.array([
+        [10, 3],
+        [3, 1]
+    ])
+    
+    t1 = x <= 0.5
+    t2 = x > 0.5
+    
+    permeability[t1,:] = k1
+    permeability[t2,:] = k2
+    return {'permeability': permeability}
+    
+
+def get_permeability_p2(n_elements, centroids=0):
+    return get_permeability_p1(n_elements)
+    
+def get_permeability_p1(n_elements, centroids=0):
     permeability = np.zeros((n_elements, 2, 2))
     K = np.array([
         [1.5, 0.5],
@@ -27,6 +85,8 @@ def get_permeability_p1(n_elements):
     return {'permeability': permeability}
 
 def run(pr_name, mesh_type, ns, n):
+    global all_pr_names
+    
     
     mesh_test_name = defpaths.load_mpfad_meshtest_by_type_and_number(mesh_type, ns[n])
     mesh_properties_name = pr_name + '_' + mesh_type + '_' + str(ns[n])
@@ -34,10 +94,22 @@ def run(pr_name, mesh_type, ns, n):
     pressure_tag = 'pressure'
     keys_prop = list(mesh_properties.keys())
     
+    
     if pressure_tag not in keys_prop:
         # define nodes to calculate_weights
         mesh_properties.insert_data({'nodes_to_calculate': mesh_properties.nodes.copy()})
         ## create weights and xi params for flux calculation
+        centroids_nodes = mesh_properties.nodes_centroids
+        z_centroids = np.zeros((len(centroids_nodes), 1))
+        centroids_nodes = np.hstack([centroids_nodes, z_centroids])
+        nodes_of_faces = mesh_properties.nodes_of_faces
+        cnodes_faces = centroids_nodes[nodes_of_faces]
+        n_faces = len(mesh_properties.faces)        
+        areas = np.zeros(n_faces)
+        for i in range(n_faces):
+            areas[i] = calculate_face_properties.polygon_area(cnodes_faces[i])
+        mesh_properties.insert_data({'areas': areas})
+        
         lsds = LsdsFluxCalculation()
         mesh_properties.update_data(
             lsds.preprocess(**mesh_properties.get_all_data())
@@ -49,9 +121,6 @@ def run(pr_name, mesh_type, ns, n):
         })
         
         mesh_properties.remove_data(['nodes_to_calculate'])
-        mesh_properties.update_data(
-            get_permeability_p1(len(mesh_properties.faces))
-        )
         
         #define boundary conditions    
         problem_name = pr_name
@@ -61,7 +130,7 @@ def run(pr_name, mesh_type, ns, n):
         xmin, ymin = mesh_properties.faces_centroids[:, 0:2].min(axis=0)
         xmax, ymax = mesh_properties.faces_centroids[:, 0:2].max(axis=0)
         
-        mesh_delta = 1e-10
+        mesh_delta = 1e-13
         nodes_xmin = mesh_properties.nodes[
             mesh_properties.nodes_centroids[:, 0] < xmin + mesh_delta
         ]
@@ -80,7 +149,26 @@ def run(pr_name, mesh_type, ns, n):
         ])).astype(np.uint64)
         
         centroids_nodes_bc = mesh_properties.nodes_centroids[nodes_bc, 0:2]
-        pressures_bc = exact_solution_p1(centroids_nodes_bc)
+        
+        if pr_name == all_pr_names[0]:
+            mesh_properties.update_data(
+                get_permeability_p1(len(mesh_properties.faces), mesh_properties.faces_centroids[:, 0:2])
+            )
+            exact_solution = exact_solution_p1
+        elif pr_name == all_pr_names[1]:
+            mesh_properties.update_data(
+                get_permeability_p2(len(mesh_properties.faces), mesh_properties.faces_centroids[:, 0:2])
+            )
+            exact_solution = exact_solution_p2
+        elif pr_name == all_pr_names[2]:
+            mesh_properties.update_data(
+                get_permeability_p3(len(mesh_properties.faces), mesh_properties.faces_centroids[:, 0:2])
+            )
+            exact_solution = exact_solution_p3
+        else:
+            raise NameError
+        
+        pressures_bc = exact_solution(centroids_nodes_bc)
         
         bc.set_boundary('nodes_pressures', nodes_bc, pressures_bc)
         
@@ -116,18 +204,22 @@ def run(pr_name, mesh_type, ns, n):
         
         error = np.absolute((pressure - p_exact))
         
+        error2 = np.power(error, 2)
+        
         mesh_properties.insert_data({
             pressure_tag: pressure,
             'edges_flux': edges_flux,
             'faces_flux': faces_flux,
             'p_exact': p_exact,
-            'error': error
+            'error': error,
+            'error2': error2
         })
         mesh_properties.export_data()
     
     l1_norm = mesh_properties.error.max()
+    # Eu = np.sqrt(np.dot(mesh_properties.areas, mesh_properties.error2))
+    # l1_norm = Eu
     l2_norm = np.linalg.norm(mesh_properties.error)
-    
     
     
     # mesh_path = os.path.join(defpaths.mesh, mesh_test_name)
@@ -151,12 +243,10 @@ def run(pr_name, mesh_type, ns, n):
     # mesh_data.export_only_the_elements('test_7_nodes_pressure_boundary', 'nodes', nodes_bc)
     
     return l1_norm, l2_norm, len(mesh_properties.faces)
-
-    
+   
         
 
-def testp1_by_meshtype(mesh_type, ns):
-    pr_name = 'problem1'
+def testp1_by_meshtype(mesh_type, ns, pr_name):
     
     all_l1_error = []
     all_l2_error = []
@@ -182,13 +272,15 @@ def testp1_by_meshtype(mesh_type, ns):
     
 def plot_errors():
     # 'mesh1': [8, 32, 64, 128]
+    global all_pr_names
+    pr_name = all_pr_names[2]
     
     mesh_types_dict = {
-        'mesh1': [8, 32, 64, 128],
-        'mesh2': [0, 1, 2, 3, 4, 5, 6, 7],
+        # 'mesh1': [8, 32, 64, 128],
+        # 'mesh2': [0, 1, 2, 3, 4, 5, 6, 7],
         'mesh5': [12, 24, 48, 96, 192, 384],
-        'mesh6': [1, 2, 3, 4]  
-    }   
+        # 'mesh6': [1, 2, 3, 4]   
+    }
     
     fig1, ax1 = plt.subplots(1)
     fig2, ax2 = plt.subplots(1)
@@ -197,23 +289,23 @@ def plot_errors():
     
     mesh_types = list(mesh_types_dict.keys())
     for mesh_type in mesh_types:
-        resp = testp1_by_meshtype(mesh_type, mesh_types_dict[mesh_type])
+        resp = testp1_by_meshtype(mesh_type, mesh_types_dict[mesh_type], pr_name)
         all_resps.append(resp.update({'mesh_type': mesh_type}))
         all_resps.append(resp.update({'mesh_type': mesh_type}))
         ax1.plot(np.log10(resp['n_faces']), np.log10(resp['l1_norm']), label=mesh_type)
         ax2.plot(np.log10(resp['n_faces']), np.log10(resp['l2_norm']), label=mesh_type)
     
     ax1.set_xlabel('N faces')
-    ax1.set_ylabel('Log10 L1 norm')
-    ax2.set_xlabel('N faces')
+    ax1.set_ylabel('Log10 Linf norm')
+    ax2.set_xlabel('Log 10 N faces')
     ax2.set_ylabel('Log10 L2 norm')
-    ax1.set_title('L1 Norm')
-    ax2.set_title('L2 Norm')
+    ax1.set_title(pr_name)
+    ax2.set_title(pr_name)
     ax1.legend()
     ax2.legend()
     
-    fig1.savefig('L1 Norm.svg', format='svg')
-    fig2.savefig('L2 Norm.svg', format='svg')
+    fig1.savefig(os.path.join('results', 'L1_Norm.svg'), format='svg')
+    fig2.savefig(os.path.join('results', 'L2_Norm.svg'), format='svg')
 
     
 
